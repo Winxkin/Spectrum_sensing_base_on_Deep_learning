62cadcc97b1a552d67909a06cb1372094df11c25
winxkin <huan2931@gmail.com>
2024-03-24 20:13:32 +0700

    Change model name

--- UnetModel/UnetE_gconv.m
+++ UnetModel/UnetE_gconv.m
@@ -1,4 +1,4 @@
-lUnetE = layerGraph();
+lUnetE_gconv = layerGraph();
 
 %% basic layer
 tempLayers = [
@@ -7,7 +7,7 @@ tempLayers = [
     reluLayer("Name","Relu_00_0")
     convolution2dLayer([3 3],64,"Name","Conv_00_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_00_1")];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = [
     maxPooling2dLayer([2 2],"Name","MaxPool_10_0","Stride",[2 2])
@@ -15,10 +15,10 @@ tempLayers = [
     reluLayer("Name","Relu_10_0")
     groupedConvolution2dLayer([3 3],128,4,"Name","Conv_10_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_10_1")];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = resize2dLayer("Name","resize-scale_10_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2]);
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_01_0")
@@ -26,7 +26,7 @@ tempLayers = [
     reluLayer("Name","Relu_01_0")
     groupedConvolution2dLayer([3 3],64,2,"Name","Conv_01_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_01_1")];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = [
     maxPooling2dLayer([2 2],"Name","MaxPool_20_0","Stride",[2 2])
@@ -34,10 +34,10 @@ tempLayers = [
     reluLayer("Name","Relu_20_0")
     groupedConvolution2dLayer([3 3],256,8,"Name","Conv_20_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_20_1")];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = resize2dLayer("Name","resize-scale_20_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2]);
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_11_0")
@@ -45,7 +45,7 @@ tempLayers = [
     reluLayer("Name","Relu_11_0")
     groupedConvolution2dLayer([3 3],128,4,"Name","Conv_11_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_11_1")];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = [
     maxPooling2dLayer([2 2],"Name","MaxPool_30_0","Stride",[2 2])
@@ -53,10 +53,10 @@ tempLayers = [
     reluLayer("Name","Relu_30_0")
     groupedConvolution2dLayer([3 3],512,16,"Name","Conv_30_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_30_1")];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = resize2dLayer("Name","resize-scale_30_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2]);
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_21_0")
@@ -65,10 +65,10 @@ tempLayers = [
     groupedConvolution2dLayer([3 3],256,8,"Name","Conv_21_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_21_1")
     resize2dLayer("Name","resize-scale_21_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2])];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = resize2dLayer("Name","resize-scale_11_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2]);
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_02_0")
@@ -76,10 +76,10 @@ tempLayers = [
     reluLayer("Name","Relu_02_0")
     groupedConvolution2dLayer([3 3],64,2,"Name","Conv_02_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_02_1")];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = dropoutLayer(0.5,"Name","dropout_40_0");
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = [
     maxPooling2dLayer([2 2],"Name","MaxPool_40_0","Stride",[2 2])
@@ -87,7 +87,7 @@ tempLayers = [
     groupedConvolution2dLayer([3 3],1024,32,"Name","Conv_40_1","Padding",[1 1 1 1])
     dropoutLayer(0.5,"Name","dropout_40_1")
     resize2dLayer("Name","resize-scale_40_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2])];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_40_0")
@@ -96,7 +96,7 @@ tempLayers = [
     groupedConvolution2dLayer([3 3],512,16,"Name","Conv_40_3","Padding",[1 1 1 1])
     reluLayer("Name","Relu_40_3")
     resize2dLayer("Name","resize-scale_40_1","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2])];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_31_0")
@@ -106,7 +106,7 @@ tempLayers = [
     reluLayer("Name","Relu_31_1")
     dropoutLayer(0.5,"Name","dropout_31_0")
     resize2dLayer("Name","resize-scale_31_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2])];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,3,"Name","concat_22_0")
@@ -115,7 +115,7 @@ tempLayers = [
     groupedConvolution2dLayer([3 3],256,8,"Name","Conv_22_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_22_1")
     resize2dLayer("Name","resize-scale_22_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2])];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_12_0")
@@ -124,7 +124,7 @@ tempLayers = [
     groupedConvolution2dLayer([3 3],128,4,"Name","Conv_12_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_12_1")
     resize2dLayer("Name","resize-scale_12_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2])];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,3,"Name","concat_13_0")
@@ -132,7 +132,7 @@ tempLayers = [
     reluLayer("Name","Relu_13_2")
     groupedConvolution2dLayer([3 3],128,4,"Name","Conv_13_3","Padding",[1 1 1 1])
     reluLayer("Name","Relu_13_3")];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_03_0")
@@ -140,7 +140,7 @@ tempLayers = [
     reluLayer("Name","Relu_03_0")
     groupedConvolution2dLayer([3 3],64,2,"Name","Conv_03_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_03_1")];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,5,"Name","concat_04_0")
@@ -153,7 +153,7 @@ tempLayers = [
     convolution2dLayer([3 3],1,"Name","Conv_04_5","Padding",[1 1 1 1])
     reluLayer("Name","Relu_04_5")
     sigmoidLayer("Name","sigmoidLayer_04_0")];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 %% classification layer
 numofclass = 3;
@@ -162,48 +162,48 @@ tempLayers = [
     fullyConnectedLayer(numofclass,"Name","fullyconnected")
     classificationLayer("Name","classoutput")
     ];
-lUnetE = addLayers(lUnetE,tempLayers);
+lUnetE_gconv = addLayers(lUnetE_gconv,tempLayers);
 
 % clean up helper variable
 clear tempLayers;
 %% Connection
-lUnetE = connectLayers(lUnetE,"Relu_00_1","MaxPool_10_0");
-lUnetE = connectLayers(lUnetE,"Relu_00_1","concat_01_0/in1");
-lUnetE = connectLayers(lUnetE,"Relu_00_1","concat_04_0/in5");
-lUnetE = connectLayers(lUnetE,"Relu_10_1","resize-scale_10_0");
-lUnetE = connectLayers(lUnetE,"Relu_10_1","MaxPool_20_0");
-lUnetE = connectLayers(lUnetE,"Relu_10_1","concat_11_0/in1");
-lUnetE = connectLayers(lUnetE,"Relu_10_1","concat_13_0/in3");
-lUnetE = connectLayers(lUnetE,"resize-scale_10_0","concat_01_0/in2");
-lUnetE = connectLayers(lUnetE,"Relu_01_1","concat_02_0/in1");
-lUnetE = connectLayers(lUnetE,"Relu_01_1","concat_04_0/in4");
-lUnetE = connectLayers(lUnetE,"Relu_20_1","resize-scale_20_0");
-lUnetE = connectLayers(lUnetE,"Relu_20_1","MaxPool_30_0");
-lUnetE = connectLayers(lUnetE,"Relu_20_1","concat_21_0/in1");
-lUnetE = connectLayers(lUnetE,"Relu_20_1","concat_22_0/in3");
-lUnetE = connectLayers(lUnetE,"resize-scale_20_0","concat_11_0/in2");
-lUnetE = connectLayers(lUnetE,"Relu_30_1","resize-scale_30_0");
-lUnetE = connectLayers(lUnetE,"Relu_30_1","dropout_40_0");
-lUnetE = connectLayers(lUnetE,"resize-scale_30_0","concat_21_0/in2");
-lUnetE = connectLayers(lUnetE,"resize-scale_30_0","concat_31_0/in2");
-lUnetE = connectLayers(lUnetE,"Relu_11_1","resize-scale_11_0");
-lUnetE = connectLayers(lUnetE,"Relu_11_1","concat_12_0/in1");
-lUnetE = connectLayers(lUnetE,"resize-scale_11_0","concat_02_0/in2");
-lUnetE = connectLayers(lUnetE,"dropout_40_0","MaxPool_40_0");
-lUnetE = connectLayers(lUnetE,"dropout_40_0","concat_40_0/in2");
-lUnetE = connectLayers(lUnetE,"resize-scale_40_0","concat_40_0/in1");
-lUnetE = connectLayers(lUnetE,"resize-scale_40_1","concat_31_0/in1");
-lUnetE = connectLayers(lUnetE,"resize-scale_31_0","concat_22_0/in1");
-lUnetE = connectLayers(lUnetE,"Relu_02_1","concat_03_0/in1");
-lUnetE = connectLayers(lUnetE,"Relu_02_1","concat_04_0/in3");
-lUnetE = connectLayers(lUnetE,"resize-scale_21_0","concat_22_0/in2");
-lUnetE = connectLayers(lUnetE,"resize-scale_21_0","concat_12_0/in2");
-lUnetE = connectLayers(lUnetE,"resize-scale_22_0","concat_13_0/in1");
-lUnetE = connectLayers(lUnetE,"resize-scale_12_0","concat_13_0/in2");
-lUnetE = connectLayers(lUnetE,"resize-scale_12_0","concat_03_0/in2");
-lUnetE = connectLayers(lUnetE,"Relu_13_3","concat_04_0/in1");
-lUnetE = connectLayers(lUnetE,"Relu_03_1","concat_04_0/in2");
-lUnetE = connectLayers(lUnetE,"sigmoidLayer_04_0","maxpooling");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_00_1","MaxPool_10_0");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_00_1","concat_01_0/in1");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_00_1","concat_04_0/in5");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_10_1","resize-scale_10_0");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_10_1","MaxPool_20_0");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_10_1","concat_11_0/in1");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_10_1","concat_13_0/in3");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"resize-scale_10_0","concat_01_0/in2");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_01_1","concat_02_0/in1");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_01_1","concat_04_0/in4");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_20_1","resize-scale_20_0");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_20_1","MaxPool_30_0");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_20_1","concat_21_0/in1");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_20_1","concat_22_0/in3");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"resize-scale_20_0","concat_11_0/in2");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_30_1","resize-scale_30_0");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_30_1","dropout_40_0");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"resize-scale_30_0","concat_21_0/in2");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"resize-scale_30_0","concat_31_0/in2");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_11_1","resize-scale_11_0");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_11_1","concat_12_0/in1");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"resize-scale_11_0","concat_02_0/in2");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"dropout_40_0","MaxPool_40_0");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"dropout_40_0","concat_40_0/in2");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"resize-scale_40_0","concat_40_0/in1");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"resize-scale_40_1","concat_31_0/in1");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"resize-scale_31_0","concat_22_0/in1");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_02_1","concat_03_0/in1");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_02_1","concat_04_0/in3");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"resize-scale_21_0","concat_22_0/in2");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"resize-scale_21_0","concat_12_0/in2");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"resize-scale_22_0","concat_13_0/in1");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"resize-scale_12_0","concat_13_0/in2");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"resize-scale_12_0","concat_03_0/in2");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_13_3","concat_04_0/in1");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"Relu_03_1","concat_04_0/in2");
+lUnetE_gconv = connectLayers(lUnetE_gconv,"sigmoidLayer_04_0","maxpooling");
 
 %% Plot
-plot(lUnetE);
+plot(lUnetE_gconv);
--- UnetModel/Unetp_gconv.m
+++ UnetModel/Unetp_gconv.m
@@ -1,4 +1,4 @@
-lUnetp = layerGraph();
+lUnetp_gconv = layerGraph();
 
 %% Define basic layer for Unet network
 
@@ -8,7 +8,7 @@ tempLayers = [
     reluLayer("Name","Relu_00_0")
     convolution2dLayer([3 3],64,"Name","Conv_00_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_00_1")];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = [
     maxPooling2dLayer([2 2],"Name","MaxPool_10_0","Stride",[2 2])
@@ -16,10 +16,10 @@ tempLayers = [
     reluLayer("Name","Relu_10_0")
     groupedConvolution2dLayer([3 3],128,4,"Name","Conv_10_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_10_1")];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = resize2dLayer("Name","resize-scale_10_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2]);
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_01_0")
@@ -27,7 +27,7 @@ tempLayers = [
     reluLayer("Name","Relu_01_0")
     groupedConvolution2dLayer([3 3],64,2,"Name","Conv_01_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_01_1")];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = [
     maxPooling2dLayer([2 2],"Name","MaxPool_20_0","Stride",[2 2])
@@ -35,10 +35,10 @@ tempLayers = [
     reluLayer("Name","Relu_20_0")
     groupedConvolution2dLayer([3 3],256,8,"Name","Conv_20_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_20_1")];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = resize2dLayer("Name","resize-scale_20_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2]);
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_11_0")
@@ -46,7 +46,7 @@ tempLayers = [
     reluLayer("Name","Relu_11_0")
     groupedConvolution2dLayer([3 3],128,4,"Name","Conv_11_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_11_1")];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = [
     maxPooling2dLayer([2 2],"Name","MaxPool_30_0","Stride",[2 2])
@@ -54,10 +54,10 @@ tempLayers = [
     reluLayer("Name","Relu_30_0")
     groupedConvolution2dLayer([3 3],512,16,"Name","Conv_30_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_30_1")];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = resize2dLayer("Name","resize-scale_30_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2]);
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_21_0")
@@ -66,10 +66,10 @@ tempLayers = [
     groupedConvolution2dLayer([3 3],256,8,"Name","Conv_21_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_21_1")
     resize2dLayer("Name","resize-scale_21_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2])];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = resize2dLayer("Name","resize-scale_11_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2]);
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_02_0")
@@ -77,10 +77,10 @@ tempLayers = [
     reluLayer("Name","Relu_02_0")
     groupedConvolution2dLayer([3 3],64,2,"Name","Conv_02_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_02_1")];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = dropoutLayer(0.5,"Name","dropout_40_0");
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = [
     maxPooling2dLayer([2 2],"Name","MaxPool_40_0","Stride",[2 2])
@@ -88,7 +88,7 @@ tempLayers = [
     groupedConvolution2dLayer([3 3],1024,32,"Name","Conv_40_1","Padding",[1 1 1 1])
     dropoutLayer(0.5,"Name","dropout_40_1")
     resize2dLayer("Name","resize-scale_40_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2])];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_40_0")
@@ -97,7 +97,7 @@ tempLayers = [
     groupedConvolution2dLayer([3 3],512,16,"Name","Conv_40_3","Padding",[1 1 1 1])
     reluLayer("Name","Relu_40_3")
     resize2dLayer("Name","resize-scale_40_1","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2])];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_31_0")
@@ -107,7 +107,7 @@ tempLayers = [
     reluLayer("Name","Relu_31_1")
     dropoutLayer(0.5,"Name","dropout_31_0")
     resize2dLayer("Name","resize-scale_31_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2])];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_22_0")
@@ -116,7 +116,7 @@ tempLayers = [
     groupedConvolution2dLayer([3 3],256,8,"Name","Conv_22_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_22_1")
     resize2dLayer("Name","resize-scale_22_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2])];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_12_0")
@@ -125,7 +125,7 @@ tempLayers = [
     groupedConvolution2dLayer([3 3],128,4,"Name","Conv_12_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_12_1")
     resize2dLayer("Name","resize-scale_12_0","GeometricTransformMode","half-pixel","Method","bilinear","NearestRoundingMode","round","Scale",[2 2])];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_13_0")
@@ -133,7 +133,7 @@ tempLayers = [
     reluLayer("Name","Relu_13_2")
     groupedConvolution2dLayer([3 3],128,4,"Name","Conv_13_3","Padding",[1 1 1 1])
     reluLayer("Name","Relu_13_3")];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,2,"Name","concat_03_0")
@@ -141,7 +141,7 @@ tempLayers = [
     reluLayer("Name","Relu_03_0")
     groupedConvolution2dLayer([3 3],64,2,"Name","Conv_03_1","Padding",[1 1 1 1])
     reluLayer("Name","Relu_03_1")];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 tempLayers = [
     concatenationLayer(3,5,"Name","concat_04_0")
@@ -154,7 +154,7 @@ tempLayers = [
     convolution2dLayer([3 3],1,"Name","Conv_04_5","Padding",[1 1 1 1])
     reluLayer("Name","Relu_04_5")
     sigmoidLayer("Name","sigmoidLayer_04_0")];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 %% classification layer
 numofclass = 3;
@@ -163,47 +163,47 @@ tempLayers = [
     fullyConnectedLayer(numofclass,"Name","fullyconnected")
     classificationLayer("Name","classoutput")
     ];
-lUnetp = addLayers(lUnetp,tempLayers);
+lUnetp_gconv = addLayers(lUnetp_gconv,tempLayers);
 
 % clean up helper variable
 clear tempLayers;
 
 %% Connection
-lUnetp = connectLayers(lUnetp,"Relu_00_1","MaxPool_10_0");
-lUnetp = connectLayers(lUnetp,"Relu_00_1","concat_01_0/in1");
-lUnetp = connectLayers(lUnetp,"Relu_00_1","concat_04_0/in5");
-lUnetp = connectLayers(lUnetp,"Relu_10_1","resize-scale_10_0");
-lUnetp = connectLayers(lUnetp,"Relu_10_1","MaxPool_20_0");
-lUnetp = connectLayers(lUnetp,"Relu_10_1","concat_11_0/in1");
-lUnetp = connectLayers(lUnetp,"resize-scale_10_0","concat_01_0/in2");
-lUnetp = connectLayers(lUnetp,"Relu_01_1","concat_02_0/in1");
-lUnetp = connectLayers(lUnetp,"Relu_01_1","concat_04_0/in4");
-lUnetp = connectLayers(lUnetp,"Relu_20_1","resize-scale_20_0");
-lUnetp = connectLayers(lUnetp,"Relu_20_1","MaxPool_30_0");
-lUnetp = connectLayers(lUnetp,"Relu_20_1","concat_21_0/in1");
-lUnetp = connectLayers(lUnetp,"resize-scale_20_0","concat_11_0/in2");
-lUnetp = connectLayers(lUnetp,"Relu_30_1","resize-scale_30_0");
-lUnetp = connectLayers(lUnetp,"Relu_30_1","dropout_40_0");
-lUnetp = connectLayers(lUnetp,"resize-scale_30_0","concat_21_0/in2");
-lUnetp = connectLayers(lUnetp,"resize-scale_30_0","concat_31_0/in2");
-lUnetp = connectLayers(lUnetp,"Relu_11_1","resize-scale_11_0");
-lUnetp = connectLayers(lUnetp,"Relu_11_1","concat_12_0/in1");
-lUnetp = connectLayers(lUnetp,"resize-scale_11_0","concat_02_0/in2");
-lUnetp = connectLayers(lUnetp,"dropout_40_0","MaxPool_40_0");
-lUnetp = connectLayers(lUnetp,"dropout_40_0","concat_40_0/in2");
-lUnetp = connectLayers(lUnetp,"resize-scale_40_0","concat_40_0/in1");
-lUnetp = connectLayers(lUnetp,"resize-scale_40_1","concat_31_0/in1");
-lUnetp = connectLayers(lUnetp,"resize-scale_31_0","concat_22_0/in1");
-lUnetp = connectLayers(lUnetp,"Relu_02_1","concat_03_0/in1");
-lUnetp = connectLayers(lUnetp,"Relu_02_1","concat_04_0/in3");
-lUnetp = connectLayers(lUnetp,"resize-scale_21_0","concat_22_0/in2");
-lUnetp = connectLayers(lUnetp,"resize-scale_21_0","concat_12_0/in2");
-lUnetp = connectLayers(lUnetp,"resize-scale_22_0","concat_13_0/in1");
-lUnetp = connectLayers(lUnetp,"resize-scale_12_0","concat_13_0/in2");
-lUnetp = connectLayers(lUnetp,"resize-scale_12_0","concat_03_0/in2");
-lUnetp = connectLayers(lUnetp,"Relu_13_3","concat_04_0/in1");
-lUnetp = connectLayers(lUnetp,"Relu_03_1","concat_04_0/in2");
-lUnetp = connectLayers(lUnetp,"sigmoidLayer_04_0","maxpooling");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_00_1","MaxPool_10_0");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_00_1","concat_01_0/in1");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_00_1","concat_04_0/in5");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_10_1","resize-scale_10_0");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_10_1","MaxPool_20_0");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_10_1","concat_11_0/in1");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"resize-scale_10_0","concat_01_0/in2");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_01_1","concat_02_0/in1");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_01_1","concat_04_0/in4");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_20_1","resize-scale_20_0");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_20_1","MaxPool_30_0");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_20_1","concat_21_0/in1");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"resize-scale_20_0","concat_11_0/in2");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_30_1","resize-scale_30_0");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_30_1","dropout_40_0");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"resize-scale_30_0","concat_21_0/in2");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"resize-scale_30_0","concat_31_0/in2");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_11_1","resize-scale_11_0");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_11_1","concat_12_0/in1");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"resize-scale_11_0","concat_02_0/in2");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"dropout_40_0","MaxPool_40_0");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"dropout_40_0","concat_40_0/in2");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"resize-scale_40_0","concat_40_0/in1");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"resize-scale_40_1","concat_31_0/in1");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"resize-scale_31_0","concat_22_0/in1");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_02_1","concat_03_0/in1");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_02_1","concat_04_0/in3");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"resize-scale_21_0","concat_22_0/in2");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"resize-scale_21_0","concat_12_0/in2");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"resize-scale_22_0","concat_13_0/in1");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"resize-scale_12_0","concat_13_0/in2");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"resize-scale_12_0","concat_03_0/in2");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_13_3","concat_04_0/in1");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"Relu_03_1","concat_04_0/in2");
+lUnetp_gconv = connectLayers(lUnetp_gconv,"sigmoidLayer_04_0","maxpooling");
 
-plot(lUnetp);
+plot(lUnetp_gconv);
 
